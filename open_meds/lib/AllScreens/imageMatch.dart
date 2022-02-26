4183bb4759c2da19a6e437b5e7e156f1490d8d5f
minangjn <74585835+minangjn@users.noreply.github.com>
2022-02-27 05:30:32 +0800

    imagematch done

--- open_meds/lib/AllScreens/imageMatch.dart
+++ open_meds/lib/AllScreens/imageMatch.dart
@@ -1,15 +1,29 @@
-import 'dart:io';
 import 'package:image/image.dart';
 import 'package:image_compare/image_compare.dart';
 import 'dart:developer';
 
+import 'dart:async';
+import 'dart:io';
+
+import 'package:flutter/services.dart' show rootBundle;
+import 'package:path_provider/path_provider.dart';
+
+Future<File> getImageFileFromAssets(String path) async {
+  final byteData = await rootBundle.load('assets/$path');
+
+  final file = File('${(await getTemporaryDirectory()).path}/$path');
+  await file.writeAsBytes(byteData.buffer.asUint8List(byteData.offsetInBytes, byteData.lengthInBytes));
+
+  return file;
+}
+
 void imageMatch() async {
-  final file1 = File('assets/Picture1.png');
-  final file2 = File('assets/Picture2.png');
+  File file1 = await getImageFileFromAssets('Picture3.png');
+  File file2 = await getImageFileFromAssets('Picture2.png');
 
   // Calculate euclidean color distance between two images
   var imageResult = await compareImages(
-      src1: file1, src2: file2, algorithm: EuclideanColorDistance(ignoreAlpha: true));
+      src1: file1, src2: file2, algorithm: IMED());
 
   log('Difference: ${imageResult * 100}%');
 }
